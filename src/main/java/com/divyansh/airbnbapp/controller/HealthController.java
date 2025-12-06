7582ed9e8ed583a9be0f7e35decec7fd93e4b92d
Divyansh9192 <divyanshdeep2004@gmail.com>
2025-12-06 20:08:53 +0530

    Render Deploy Fix

--- src/main/java/com/divyansh/airbnbapp/controller/HealthController.java
+++ src/main/java/com/divyansh/airbnbapp/controller/HealthController.java
@@ -1,13 +1,14 @@
 package com.divyansh.airbnbapp.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
-    @GetMapping("/")
-    public String health() {
-        return "OK";
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("ok");
     }
 }
